d1f1c8caf2dd579c230780723fc0cf617cfc6386
Sidney Aulakh <sidneyaulakh@gmail.com>
2024-03-19 18:07:20 +1100

    Initial Commit

---
+++ LearningSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearningSwift
+//
+//  Created by Sidak Singh Aulakh on 19/3/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearningSwift/LearningSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftApp.swift
+//  LearningSwift
+//
+//  Created by Sidak Singh Aulakh on 19/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
