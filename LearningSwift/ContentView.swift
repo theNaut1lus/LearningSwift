0288813202837e611b4b90049c22178c559db684
Sidney Aulakh <sidneyaulakh@gmail.com>
2024-03-25 23:55:00 +1100

    class 19/03

--- LearningSwift/ContentView.swift
+++ LearningSwift/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var text = "Lightning"
+    @State var toggle = false
+    @State var string = ""
+    @State var list: [String] = []
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Rectangle()
+                .fill(.white)
+                .ignoresSafeArea()
+            VStack {
+                ForEach(list, id:\.self) {
+                    thing in Text(thing)
+                }
+                Circle()
+                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                Text(text)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundStyle(.black)
+                Button("button") {
+                    print("hello world")
+                }
+                TextField("Text", text: $string)
+                Button(action: {
+                    print(string)
+                    print("hello lightning!")
+                    toggle.toggle()
+                    text = (toggle) ? "Lightning" : "Bolt"
+                    list.append(string)
+                    print(list)
+                }, label: {
+                    Image(systemName: "bolt.heart.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 100)
+                        .foregroundStyle(.yellow)
+                })
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(.capsule)
+                .tint(.orange) // change color for a button, use tint, everything else uses forgroundStyle
+                
+            }
         }
-        .padding()
     }
 }
 
